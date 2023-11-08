9f6306e52fbd30fd64a2bd0fbffb1576d44ee9d9
MariaKim1 <mariakimpersson@gmail.com>
2023-11-08 12:39:20 +0100

    added questions - to organize my thought process

--- index_2.js
+++ index_2.js
@@ -33,27 +33,25 @@ something that is not valid.
 // ========= OBJECT ============
 const account = {
     name: "Maria",
-    expenses: 0,
-    income: 0,
+    expenses: [],
+    income: [],
 
-    addExpenses: function () {
+    addExpences: function () {
         this.expenses.push();
-    }
+// push to add something to this variable... what are my thoughts exactly..!?!
+    },
     addIncome: function () {
         this.income.push();
-    }
+    },
     listAllExpenses: function () {
         alert(this.expences);
-    } 
+// I need to be able to store NEW VALUES that the user put into the prompt. 
+// Could I do that by adding expenses (what and how much) to two separet arrays and then for-loop them into a two column list??
+    }, 
 }
 
 
 
-
-
-
-
-//  
 //const amount = parseFloat(prompt(th"How much was your expense?"));
 
 // ========= Function to hold if/else statements ============
@@ -64,12 +62,18 @@ function menu() {
         "START TRACKING YOUR EXPENCES!\nPlease select a nr from the menu:\n1) Add income\n2) Add expense\n3) List of all expenses\n4) See total balance"
         ));
 
-    if (menuChoice === 1) {
+    if (menuChoice === "1") {
         prompt("Add your monthly income after tax:");
+// Need to store this in a variable to be able to reuse it in a calculation later.
         } else if (menuChoice === "2") {
             prompt("What was your expense? (food, rent, etc)\n");
+            prompt("How much was this expence on XX?");
+// Need to store both these values in different variable to be able to reuse it in an alert "List of all expences" - a list of two columns.
+// Can I use the for Loop here somehow?
         } else if (menuChoice === "3") {
-            prompt("List of all expenses:\n");
+            alert("List of all expenses:\n");
+// alert: "List of all expences" - a list of two columns.
+// Can I stor this in the object? shall it be a function? How do I call it/make this code relate to the account-object?
         } else if (menuChoice === "4") {
             alert("Total balance:\n");
         }
